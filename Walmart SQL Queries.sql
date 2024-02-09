290cfc80e12cd3ee1451460faf9851de841edd8a
Rishikesh Gundla <85453865+rishigundla@users.noreply.github.com>
2024-02-09 23:13:12 +0530

    Add files via upload

--- Walmart SQL Queries.sql
+++ Walmart SQL Queries.sql
@@ -1,3 +1,45 @@
+/*
+About
+This project aims to explore the Walmart Sales data to understand top performing branches and products, sales trend of of different products, customer behaviour. 
+The aims is to study how sales strategies can be improved and optimized. The dataset was obtained from the Kaggle Walmart Sales Forecasting Competition.
+
+"In this recruiting competition, job-seekers are provided with historical sales data for 45 Walmart stores located in different regions. Each store contains many departments, 
+and participants must project the sales for each department in each store. To add to the challenge, selected holiday markdown events are included in the dataset. 
+These markdowns are known to affect sales, but it is challenging to predict which departments are affected and the extent of the impact." source
+
+Purposes Of The Project
+The major aim of thie project is to gain insight into the sales data of Walmart to understand the different factors that affect sales of the different branches.
+
+Analysis List:-
+1. Product Analysis
+Conduct analysis on the data to understand the different product lines, the products lines performing best and the product lines that need to be improved.
+
+2. Sales Analysis
+This analysis aims to answer the question of the sales trends of product. The result of this can help use measure the effectiveness of each sales strategy the business applies 
+and what modificatoins are needed to gain more sales.
+
+3. Customer Analysis
+This analysis aims to uncover the different customers segments, purchase trends and the profitability of each customer segment.
+
+Approach Used:-
+1. Data Wrangling: This is the first step where inspection of data is done to make sure NULL values and missing values are detected and data replacement methods are used to replace, missing or NULL values.
+	a) Build a database
+	b) Create table and insert the data.
+	c) Select columns with null values in them. There are no null values in our database as in creating the tables, we set NOT NULL for each field, hence null values are filtered out.
+    
+2. Feature Engineering: This will help use generate some new columns from existing ones.
+	a) Add a new column named time_of_day to give insight of sales in the Morning, Afternoon and Evening. This will help answer the question on which part of the day most sales are made.
+    
+	b) Add a new column named day_name that contains the extracted days of the week on which the given transaction took place (Mon, Tue, Wed, Thur, Fri). 
+       This will help answer the question on which week of the day each branch is busiest.
+    
+	c) Add a new column named month_name that contains the extracted months of the year on which the given transaction took place (Jan, Feb, Mar). 
+	   Help determine which month of the year has the most sales and profit.
+    
+3. Exploratory Data Analysis (EDA): Exploratory data analysis is done to answer the listed questions and aims of this project.
+*/
+
+
 -- Create database
 CREATE DATABASE IF NOT EXISTS walmart;
 
